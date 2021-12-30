c654868702fb64b956d7d07dce617ea76d0ea302
Pol <mironpaul2019@gmail.com>
2021-12-30 12:35:49 +0200

    plm

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
-const app = require("express")();
+let express = require("express");
+let app = express();
 
 const PORT = process.env.PORT || 3000;
-
+app.use(express.static(__dirname))
 app.get("" , (req, res) => {
-    res.sendFile(__dirname + "/index.html");
+    res.sendFile("index.html");
+    res.sendFile("./.well-known/pki-validation/CC8ECBF936742C60B0E2F4AC686AE6CE.txt")
 })
 
 app.listen(PORT, () => {
